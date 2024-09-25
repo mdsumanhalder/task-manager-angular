9ce6f642513a31d0550a50bc8e7c0189d44e3b93
suman halder <sumanhalder@sumans-Air.lan>
2024-09-25 22:41:05 +0200

    updated yml file

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+// src/environments/environment.prod.ts
+
+export const environment = {
+  production: true,
+  apiUrl: 'https://your-production-api-url.com', // Replace this with your actual production API URL
+  otherConfig: 'value', // Add other production-specific configurations
+};
